1b370c9311365b0f52becff9faf403e73e6b49cd
parksomin72 <fatimazahraealawi@gmail.com>
2024-04-18 11:19:11 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return their sum as a float.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
+
+if __name__ == "__main__":
+    add(1.11, 2.22)
